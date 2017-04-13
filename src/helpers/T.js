89c848ee5aac3ccf3a62824cbaaa24b5c2b3c6b2
spences10 <spences10apps@gmail.com>
2017-04-13 12:43:26 +0100

    clear down :see_no_evil:

--- src/helpers/T.js
+++
@@ -1,6 +0,0 @@
-const Twit = require('twit')
-const config = require('../config')
-
-const T = new Twit(config)
-
-module.exports = T
--- src/helpers/queryStrings.js
+++
@@ -1,7 +0,0 @@
-const strings = require('./strings')
-
-let hashtags = strings.searchQueryStrings.join(' OR ')
-
-hashtags = '#100daysofcode OR #301daysofcode'
-
-module.exports = hashtags
