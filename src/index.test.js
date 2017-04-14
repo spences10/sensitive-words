44b7e991d572d2efe7e49afc58e252a51a3a2be0
spences10 <spences10apps@gmail.com>
2017-04-14 06:41:57 +0100

    publish and bump release number

--- src/index.test.js
+++ src/index.test.js
@@ -4,12 +4,12 @@ test('replace blacklisted words with asterisks', () => {
   expect(sensitiveWords(
     'The name of the NX will be the Nintendo Switch', 
     ['switch']
-  )).toBe('The name of the NX will be the Nintendo ***')
+  )).toBe('The name of the NX will be the Nintendo ****')
 })
 
 test('replaces multiple instances of blacklisted words', () => {
   expect(sensitiveWords(
     'The name of the NX will be the Nintendo Switch. The switch will be awesome!', 
     ['switch']
-  )).toBe('The name of the NX will be the Nintendo ***. The *** will be awesome!')
+  )).toBe('The name of the NX will be the Nintendo ****. The **** will be awesome!')
 })
