baffb4373283c14f91a7b71377a7d312abe32602
spences10 <spences10apps@gmail.com>
2017-04-14 07:13:47 +0100

    add additional asterisks

--- build/index.test.js
+++ build/index.test.js
@@ -7,9 +7,9 @@ var _2 = _interopRequireDefault(_);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 test('replace blacklisted words with asterisks', function () {
-  expect((0, _2.default)('The name of the NX will be the Nintendo Switch', ['switch'])).toBe('The name of the NX will be the Nintendo ***');
+  expect((0, _2.default)('The name of the NX will be the Nintendo Switch', ['switch'])).toBe('The name of the NX will be the Nintendo ****');
 });
 
 test('replaces multiple instances of blacklisted words', function () {
-  expect((0, _2.default)('The name of the NX will be the Nintendo Switch. The switch will be awesome!', ['switch'])).toBe('The name of the NX will be the Nintendo ***. The *** will be awesome!');
+  expect((0, _2.default)('The name of the NX will be the Nintendo Switch. The switch will be awesome!', ['switch'])).toBe('The name of the NX will be the Nintendo ****. The **** will be awesome!');
 });
